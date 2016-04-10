68f0f03c7f77489357097f1d7e01b055f5a0c0f0
Alex <travnick@gmail.com>
2016-04-10 15:45:50 +0200

    table code

--- app/js/model/core/ByteArrayUtils.js
+++ app/js/model/core/ByteArrayUtils.js
@@ -2,6 +2,7 @@
  * Created by Alex on 10/04/2016.
  */
 
+'use strict';
 function writeUint8( buffer, byteOffset, value ) {
 
 }
@@ -41,3 +42,71 @@ function writeFloat32( buffer, byteOffset, value ) {
 function writeFloat64( buffer, byteOffset, value ) {
 
 }
+
+function readUint8( buffer, byteOffset, value ) {
+
+}
+
+function readUint16( buffer, byteOffset, value ) {
+
+}
+
+function readUint32( buffer, byteOffset, value ) {
+
+}
+
+function readUint64( buffer, byteOffset, value ) {
+
+}
+
+function readInt8( buffer, byteOffset, value ) {
+
+}
+
+function readInt16( buffer, byteOffset, value ) {
+
+}
+
+function readInt32( buffer, byteOffset, value ) {
+
+}
+
+function readInt64( buffer, byteOffset, value ) {
+
+}
+
+function readFloat32( buffer, byteOffset, value ) {
+
+}
+
+function readFloat64( buffer, byteOffset, value ) {
+
+}
+
+module.exports ={
+    writeUint8: writeUint8,
+    writeUint16: writeUint16,
+    writeUint32: writeUint32,
+    writeUint64: writeUint64,
+
+    writeInt8: writeInt8,
+    writeInt16: writeInt16,
+    writeInt32: writeInt32,
+    writeInt64: writeInt64,
+
+    writeFloat32: writeFloat32,
+    writeFloat64: writeFloat64,
+    
+    readUint8: readUint8,
+    readUint16: readUint16,
+    readUint32: readUint32,
+    readUint64: readUint64,
+
+    readInt8: readInt8,
+    readInt16: readInt16,
+    readInt32: readInt32,
+    readInt64: readInt64,
+
+    readFloat32: readFloat32,
+    readFloat64: readFloat64
+};
--- app/js/model/tomorrow/RowFirstTable.js
+++ app/js/model/tomorrow/RowFirstTable.js
@@ -2,9 +2,68 @@
  * Created by Alex on 10/04/2016.
  */
 
-var RowFirstTable = function RowFirstTable(  ) {
-  this.channels = [];  
+var RowFirstTable = function RowFirstTable() {
+    /**
+     *
+     * @type {Array.<DataType>}
+     */
+    this.types = [];
+
+    this.data = new ArrayBuffer( 0 );
+
+    this.bytesPerRecord = 0;
+
+    this.rowsUsed = 0;
+    this.rowsAvailable = 0;
 };
+
+RowFirstTable.prototype.setActualSize = function ( rowCount ) {
+    var oldData = this.data;
+
+    this.data = new ArrayBuffer( rowCount );
+
+    var newArray = new Uint8Array( this.data );
+    var oldArray = new Uint8Array( oldData );
+
+    newArray.set( oldArray, 0 );
+
+    this.rowsAvailable = rowCount;
+};
+
+RowFirstTable.prototype.resize = function ( rowCount ) {
+    if ( this.rowsAvailable < rowCount ) {
+        //grow
+        var growFactor = 1.2;
+        var newSize = rowCount * growFactor;
+        this.setActualSize( newSize );
+    } else if ( this.rowsAvailable * 0.7 > rowCount ) {
+        //shrink
+        this.setActualSize( rowCount );
+    }
+};
+
 RowFirstTable.prototype.addRow = function ( values ) {
-    
+    var types = this.types;
+    var numChannels = types.length;
+
+    var newRowCount = this.rowsUsed+1;
+
+    this.resize(newRowCount);
+
+    var rowIndex = this.rowsUsed;
+
+    this.rowsUsed = newRowCount;
+
+    var dataView = new DataView(this.data, this.bytesPerRecord*rowIndex);
+
+    var byteOffset = 0;
+    for ( var i = 0; i < numChannels; i++ ) {
+        var type = types[ i ];
+        var value = values[i];
+
+        var setMethodName = "set"+type;
+        dataView[setMethodName]();
+    }
+
+
 };
