42fa2d7682f7497ef37a49bef4d5ea31e3409661
tyio <rodion.hagin@gmail.com>
2016-04-10 15:42:09 +0200

    new methods for dataframe added: - add channel - remove channel

--- app/js/model/tomorrow/DataFrame.js
+++ app/js/model/tomorrow/DataFrame.js
@@ -20,4 +20,26 @@ var DataFrame = function () {
     this.masterChannel = null;
 };
 
+/**
+ * Adds a new channel
+ * @param channel channel to be added
+ */
+DataFrame.prototype.addChannel = function (channel) {
+    this.channels.push(channel);
+};
+
+/**
+ * Removes channel by its id
+ * @param id id of the channel to be removed
+ */
+DataFrame.prototype.removeChannel = function(id) {
+    for (var i = 0; i < this.channels.length; i++) {
+        var channel = this.channels[i];
+        if (channel.id === id) {
+            this.channels.splice(i, 1);
+            return channel;
+        }
+    }
+};
+
 module.exports = DataFrame;
