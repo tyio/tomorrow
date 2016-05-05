eaf94872c423d616846266034176b70766be37c3
Alex <travnick@gmail.com>
2016-05-05 17:30:04 +0200

    GraphBuilder introduction, to simplify creation of a chart

--- app/js/main.js
+++ app/js/main.js
@@ -1,20 +1,22 @@
-var DataType = require( './model/tomorrow/data/DataType');
-var Channel = require( './model/tomorrow/channel/Channel');
-var DataFrame = require( './model/tomorrow/data/DataFrame');
-var Grid = require( './model/tomorrow/Grid');
-var Axis = require( './model/tomorrow/axis/Axis');
-var AxisScale = require( './model/tomorrow/axis/AxisScale');
-var Rectangle = require( './model/core/geom/Rectangle');
-var Vector2 = require( './model/core/geom/Vector2');
-var Orientation = require( './model/tomorrow/axis/Orientation');
+var DataType = require( './model/tomorrow/data/DataType' );
+var Channel = require( './model/tomorrow/channel/Channel' );
+var DataFrame = require( './model/tomorrow/data/DataFrame' );
+var Grid = require( './model/tomorrow/Grid' );
+var Axis = require( './model/tomorrow/axis/Axis' );
+var AxisScale = require( './model/tomorrow/axis/AxisScale' );
+var Rectangle = require( './model/core/geom/Rectangle' );
+var Vector2 = require( './model/core/geom/Vector2' );
+var Orientation = require( './model/tomorrow/axis/Orientation' );
 
-var Chart = require( './view/Chart');
-var ChartCanvas = require( './view/ChartCanvas');
-var ChannelView = require( './view/ChannelView');
-var ChannelStyle = require( './view/ChannelStyle');
-var GridView = require( './view/GridView');
-var AxisView = require( './view/axis/AxisView');
-var AxisScaleView = require( './view/axis/AxisScaleView');
+var Chart = require( './view/Chart' );
+var ChartCanvas = require( './view/ChartCanvas' );
+var ChannelView = require( './view/ChannelView' );
+var ChannelStyle = require( './view/ChannelStyle' );
+var GridView = require( './view/GridView' );
+var AxisView = require( './view/axis/AxisView' );
+var AxisScaleView = require( './view/axis/AxisScaleView' );
+
+var GraphBuilder = require( './model/tomorrow/GraphBuilder' );
 
 var time = new Channel();
 time.name = 'time';
@@ -26,102 +28,25 @@ temperature.name = 'temperature';
 temperature.unit = 'C';
 temperature.dataType = DataType.Int8;
 
-var dataFrame = new DataFrame([time,temperature], time);
+var dataFrame = new DataFrame( [ time, temperature ], time );
 
 function generateData( numSamples, table ) {
-    console.time("generateData");
+    console.time( "generateData" );
     var row = [];
-    for (var i = 0; i < numSamples; i++) {
-        row[0] = i/100;
-        row[1] = Math.random()*10;
-        table.addRow(row);
+    for ( var i = 0; i < numSamples; i++ ) {
+        row[ 0 ] = i / 100;
+        row[ 1 ] = Math.random() * 10;
+        table.addRow( row );
     }
-    console.timeEnd("generateData");
+    console.timeEnd( "generateData" );
 }
 
-generateData(100, dataFrame.data);
+generateData( 100, dataFrame.data );
 
-var timeAxisScale = new AxisScale();
-timeAxisScale.name = time.name;
-timeAxisScale.units = time.unit;
-timeAxisScale.markOffset = 1;
-timeAxisScale.markStride = 1;
-timeAxisScale.markStrideSkip = 0;
-timeAxisScale.markStrideFill = 0;
+var builder = new GraphBuilder();
 
-var temperatureAxisScale = new AxisScale();
-temperatureAxisScale.name = temperature.name;
-temperatureAxisScale.units = temperature.unit;
-temperatureAxisScale.markOffset = 1;
-temperatureAxisScale.markStride = 1;
-temperatureAxisScale.markStrideSkip = 0;
-temperatureAxisScale.markStrideFill = 0;
+var chart = builder.setSize( 800, 600 )
+    .setSelection( 0, 0, 1, 10 )
+    .build( dataFrame );
 
-var timeAxis = new Axis();
-timeAxis.addScale(timeAxisScale);
-
-var temperatureAxis = new Axis();
-temperatureAxis.addScale(temperatureAxisScale);
-
-var grid = new Grid();
-grid.addXAxisScale(timeAxisScale);
-grid.addYAxisScale(temperatureAxisScale);
-
-/////////////////////////////////////////////
-var channelView = new ChannelView();
-channelView.channel = temperature;
-channelView.style = new ChannelStyle();
-channelView.style.thickness = 1;
-channelView.style.lineStyle = 'solid';
-channelView.style.color = '#800080';
-
-var selection = new Rectangle(0, 0, 1, 10);
-var size = new Vector2(800, 600);
-
-var chartCanvas = new ChartCanvas({
-    size: size,
-    selection: selection,
-    dataFrame: dataFrame,
-    channelViews: [channelView]
-});
-
-var timeAxisScaleView = new AxisScaleView({
-    size: size,
-    selection: selection,
-    orientation: Orientation.HORIZONTAL,
-    axisScale: timeAxisScale
-});
-var timeAxisView = new AxisView({
-    size: size,
-    selection: selection,
-    orientation: Orientation.HORIZONTAL,
-    axis: timeAxis,
-    axisScaleViews: [timeAxisScaleView]
-});
-
-var temperatureAxisScaleView = new AxisScaleView({
-    size: size,
-    selection: selection,
-    orientation: Orientation.VERTICAL,
-    axisScale: temperatureAxisScale
-});
-
-var temperatureAxisView = new AxisView({
-    size: size,
-    selection: selection,
-    orientation: Orientation.VERTICAL,
-    axis: temperatureAxis,
-    axisScaleViews: [temperatureAxisScaleView]
-});
-
-var gridView = new GridView();
-gridView.grid = grid;
-
-var chart = new Chart();
-chart.gridView = gridView;
-chart.selection = selection;
-chart.setChartCanvas(chartCanvas);
-chart.addAxisView(timeAxisView);
-chart.addAxisView(temperatureAxisView);
-
-document.body.appendChild(chart.el);
+document.body.appendChild( chart.el );
---
+++ app/js/model/tomorrow/GraphBuilder.js
@@ -0,0 +1,149 @@
+/**
+ * Created by Alex on 05/05/2016.
+ */
+var Rectangle = require( '../core/geom/Rectangle' );
+var Vector2 = require( '../core/geom/Vector2' );
+
+var AxisScale = require( './axis/AxisScale' );
+var Axis = require( './axis/Axis' );
+
+var AxisView = require( '../../view/axis/AxisView' );
+var AxisScaleView = require( '../../view/axis/AxisScaleView' );
+
+var Grid = require( './Grid' );
+var GridView = require( '../../view/GridView' );
+
+var Chart = require( '../../view/Chart' );
+var ChartCanvas = require( '../../view/ChartCanvas' );
+
+var ChannelView = require( '../../view/ChannelView' );
+var ChannelStyle = require( '../../view/ChannelStyle' );
+
+var Orientation = require( './axis/Orientation' );
+
+var ColorUtils = require( '../core/ColorUtils' );
+
+var GraphBuilder = function () {
+    this.selection = new Rectangle( 0, 0, 1, 10 );
+    this.size = new Vector2( 800, 600 );
+};
+
+GraphBuilder.prototype.setSize = function ( x, y ) {
+    this.size.set( x, y );
+    return this;
+};
+
+GraphBuilder.prototype.setSelection = function ( x, y, width, height ) {
+    this.selection.set( x, y, width, height );
+    return this;
+};
+
+function buildAxisView( axis, orientation, size, selection ) {
+
+    var scaleViews = axis.scales.map( function ( scale ) {
+        return new AxisScaleView( {
+            size : size,
+            selection : selection,
+            orientation : orientation,
+            axisScale : scale
+        } );
+    } );
+
+    var axisView = new AxisView( {
+        size : size,
+        selection : selection,
+        orientation : orientation,
+        axis : axis,
+        axisScaleViews : scaleViews
+    } );
+
+    return axisView;
+}
+
+/**
+ *
+ * @param {Channel} channel
+ * @returns {Axis}
+ */
+function buildAxis( channel ) {
+
+    var scale = new AxisScale();
+
+    scale.name = channel.name;
+    scale.units = channel.unit;
+
+    scale.markOffset = 1;
+    scale.markStride = 1;
+    scale.markStrideSkip = 0;
+    scale.markStrideFill = 0;
+
+
+    var axis = new Axis();
+    axis.addScale( scale );
+
+    return axis;
+}
+
+/**
+ *
+ * @param {DataFrame} dataFrame
+ * @returns {Chart}
+ */
+GraphBuilder.prototype.build = function ( dataFrame ) {
+    var channels = dataFrame.channels;
+
+    var channelViews = channels.map( function ( channel, index ) {
+        var hue = index/(channels.length);
+        var color = ColorUtils.hsv2rgb( hue, 0.7, 0.9 );
+
+        var channelView = new ChannelView();
+        channelView.channel = channel;
+        channelView.style = new ChannelStyle();
+        channelView.style.thickness = 1;
+        channelView.style.lineStyle = 'solid';
+        channelView.style.color = ColorUtils.rgb2hex( color.r, color.g, color.b );
+
+        return channelView;
+    } );
+
+    var chartCanvas = new ChartCanvas( {
+        size : this.size,
+        selection : this.selection,
+        dataFrame : dataFrame,
+        channelViews : channelViews
+    } );
+
+    //use first two channels as X and Y axis respectively
+    if ( channels.length < 2 ) {
+        throw new Error( 'Insufficient number of channels to build graph, need at least 2' );
+    }
+
+    //build axis
+    var c0 = channels[ 0 ];
+    var c1 = channels[ 1 ];
+
+    var a0 = buildAxis( c0 );
+    var a1 = buildAxis( c1 );
+
+    var av0 = buildAxisView( a0, Orientation.HORIZONTAL, this.size, this.selection );
+    var av1 = buildAxisView( a1, Orientation.VERTICAL, this.size, this.selection );
+
+    var grid = new Grid();
+    grid.addXAxisScale( a0.scales[ 0 ] );
+    grid.addYAxisScale( a1.scales[ 0 ] );
+
+    var gridView = new GridView();
+    gridView.grid = grid;
+
+    var chart = new Chart();
+    chart.gridView = gridView;
+    chart.selection = this.selection;
+    chart.setChartCanvas( chartCanvas );
+
+    chart.addAxisView( av0 );
+    chart.addAxisView( av1 );
+
+    return chart;
+};
+
+module.exports = GraphBuilder;
